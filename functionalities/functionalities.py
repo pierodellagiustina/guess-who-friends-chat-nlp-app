88bc2f342bd63f2012b6ac9557830ddc59697cca
Piero Della Giustina <pierodellag@gmail.com>
2022-02-24 22:49:15 +0100

    Add the NN results

--- functionalities/functionalities.py
+++ functionalities/functionalities.py
@@ -1,6 +1,11 @@
 import pandas as pd
-import random
 import config.config as c
+import re
+from nltk.tokenize.casual import TweetTokenizer
+import string
+import numpy as np
+import pickle
+from tensorflow.keras.models import load_model
 
 
 def load_data():
@@ -68,5 +73,74 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
+
+
+def process_message(text):
+    """Simple processor to prepare sentence for the NN model"""
+
+    # replace URLs (if there are any left)
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '<<LINK>>', text)
+
+    # tokenize, lower case and remove punctuation
+    text_l = [item.lower() for item in TweetTokenizer().tokenize(text) if item not in string.punctuation]
+
+    return text_l
+
+
+def load_embedding_vocab():
+    with open('data/embedding_vocab.pkl', 'rb') as handle:
+        embedding_vocab = pickle.load(handle)
+    return embedding_vocab
+
+
+def get_nn_predictions(sentence, embedding_vocab, model, sender_mapper_reverse):
+    """Get input sentence, cleanse it and prepare it for the model predictions and return output prob distribution"""
+    max_length = 30
+
+    # sanitize
+    sentence = process_message(sentence)
+
+    # if there are no sanitized words, make a list of 0s and
+    # add it to X
+    if len(sentence) == 0:
+        x = np.array([0] * max_length)
+    else:
+
+        word_ids = []
+
+        for word in sentence:
+
+            # if it is a rare word, set it to the placeholder value
+            if word not in embedding_vocab.keys():
+                word = '<<RARE>>'
+
+            # append the id of the word to the list
+            word_ids.append(embedding_vocab[word])
+
+        # padding
+        if len(word_ids) < max_length:
+            word_ids = word_ids + [0] * (max_length - len(word_ids))
+        # shorten the sentence to the first max_length words
+        elif len(word_ids) > max_length:
+            word_ids = word_ids[:max_length]
+
+        # add to x
+        x = np.array(word_ids)
+
+    x.shape = (max_length, 1)
+    preds = model.predict(x.T).squeeze()
+
+    # get the chosen sender (i.e. max estimated prob)
+    chosen_sender = sender_mapper_reverse[preds.argmax()]
+
+    # get whole distribution and return it
+    full_distr = {sender_mapper_reverse[i]:p for i, p in enumerate(preds)}
+
+    return chosen_sender, full_distr
+
+
+def load_nn_model():
+    return load_model('data/nn_classifier/')
+
 
 
